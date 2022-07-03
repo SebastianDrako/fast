3b87cb6d14d5ee03d035195f4cd2c9aad857bbaa
Master Dragon <45272782+SebastianDrako@users.noreply.github.com>
2022-07-02 22:07:18 -0500

    Add files via upload

---
+++ IA.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  2 21:21:43 2022
+
+@author: long
+"""
+import csv
+import tensorflow as tf
+import numpy as np
+
+file = open('rem1.csv')
+csvreader = csv.reader(file)
+rows = []
+
+time = []
+num = []
+mem = []
+
+for row in csvreader:
+        rows.append(row)
+
+file.close()
+
+
+for row in rows:
+    try:
+        time.append(int(row[0]))
+    except:
+        pass
+    try:
+        num.append(int(row[0]))
+    except:
+        pass
+    try:
+        mem.append(int(row[0]))
+    except:
+        pass
+
+del row, rows     
+
+
+core = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=4 , input_shape=[1]),
+    tf.keras.layers.Dense(units=3),
+    tf.keras.layers.Dense(units=3),
+    tf.keras.layers.Dense(units=2)
+    ])
+
+core.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+
+
+
+
+historial = core.fit(mem, [time , num], epochs=1, verbose=False)
